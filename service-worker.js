8947eb039bf2ee4dea8b5d986bfa78013bf98017
infantrie111 <aufaopexck@gmail.com>
2026-01-23 04:56:04 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dewa-ban-v27'; // v3.3.5 - FINAL: Ghost container fix & global safety net
+const CACHE_NAME = 'dewa-ban-v29'; // v3.3.7 - FINAL: Dominant Brand List & Landscape Clean-up
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
